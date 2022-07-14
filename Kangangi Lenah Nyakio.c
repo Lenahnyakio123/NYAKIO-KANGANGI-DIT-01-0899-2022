8471a535f58cf88e4878a0327a01143f1a199851
Lenahnyakio123 <106369272+Lenahnyakio123@users.noreply.github.com>
2022-07-14 10:36:57 +0300

    Add files via upload

---
+++ Kangangi Lenah Nyakio.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <C>
+
+int main()
+{
+    //variable declaration;
+    int price;
+    chart price;
+
+    //input
+    printf("price system\n");
+    printf ( "enter the price sh 40-300:");
+    scanf ( "%d,price");
+    if price <40//price >300;
+    printf( invalid price/n);
+    return 0;
+}
+    //processing
+    if (price> 40){
+    item=beef smokie
+    }
+    else if (price> 70){
+    item=coffee
+    }
+    else if (price>90){
+    item=Hotdog
+    }
+    else if(price >150){
+    item=milkshake
+    }
+     else if(price >150){
+    item=french fries
+     }
+     else if ( price >300){
+     item= humburger
+     }
+     return=0
+
+
+
